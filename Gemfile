a2192979cc8f8620728dc95f2f6af9966eefff4a
hirotsuru <hirofumi.tsuruta.314@gmail.com>
2016-02-17 13:16:06 +0000

    For heroku

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bcrypt', '~> 3.1.7'
 #画像のアップロード
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'cloudinary'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,11 +7,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  def public_id
+    model.id
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
